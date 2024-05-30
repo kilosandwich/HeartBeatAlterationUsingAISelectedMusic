175ec443020422f620fe3a825e7e964626275a19
Phillip Wandyez <wandyez@gmail.com>
2024-05-30 17:25:11 -0400

    Fixed the GetCharacteristicsfunction

--- GetCharacteristics.py
+++ GetCharacteristics.py
@@ -5,15 +5,15 @@ def load_audio_segment(audioInfo, samplingRate, startTime, durationTime):
     return audioInfo[int(startTime * samplingRate):int((startTime + durationTime) * samplingRate)]
 
 def compute_tempo(audioInfo, samplingRate):
-    return librosa.beat.beat_track(audioInfo, samplingRate=samplingRate)[0]
+    return librosa.beat.beat_track(y=audioInfo, sr=samplingRate)[0]
 
 def compute_average_pitch(audioInfo, samplingRate):
-    pitches, _ = librosa.piptrack(y=audioInfo, samplingRate=samplingRate)
+    pitches, _ = librosa.piptrack(y=audioInfo, sr=samplingRate)
     return np.mean(pitches[pitches > 0])
 
 def get_characteristics(filepath):
     y, samplingRate = librosa.load(filepath)
-    songLength = librosa.get_duration(y=y, samplingRate=samplingRate)
+    songLength = librosa.get_duration(y=y, sr=samplingRate)
     avgTempo = compute_tempo(y, samplingRate)
     first_30s_audio = load_audio_segment(y, samplingRate, 0, 30)
     last_30s_audio = load_audio_segment(y, samplingRate, max(0, songLength - 30), 30)
@@ -24,16 +24,17 @@ def get_characteristics(filepath):
     pitch_last_30 = compute_average_pitch(last_30s_audio, samplingRate)
 
     return [
-        songLength,
         avgTempo,
         tempo_first_30,
         tempo_last_30,
-        avgPitch,
+        songLength,
         pitch_first_30,
-        pitch_last_30
+        pitch_last_30,
+        avgPitch
     ]
 
+#TEST IT
 if __name__ == "__main__":
-    filepath = ""
+    filepath = "Nixon.mp3"
     characteristics = get_characteristics(filepath)
     print(characteristics)
