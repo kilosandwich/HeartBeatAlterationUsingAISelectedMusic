8c8674dd00a6d6e34b2ed3c582584f4a745cc761
Jeremy-John <143729558+Jeremy-John@users.noreply.github.com>
2024-06-03 22:38:50 -0400

    Add files via upload
    Here are the most recently updated files for the front end

--- webapp.py
+++ webapp.py
@@ -1,37 +1,62 @@
-"""this is the main file for the front end it will load the webpage with the list of mp3 files from the given directory. 
- It will also create a csv with the getcharactersitics, in the rootpath function put the directory that has the music file
-you want to use. Also put the directory path in the if __name__ == "__main__": for the directory path."""
-
-from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
 import os
 import glob
-import librosa
-import numpy as np
 import csv
-import webbrowser 
+import time
+import webbrowser
 from threading import Timer
+import numpy as np
+import torch
+import librosa
+from flask import Flask, render_template, request, jsonify
+from flask_bootstrap import Bootstrap
+from openant.easy.node import Node
+from openant.devices import ANTPLUS_NETWORK_KEY
+from openant.devices.heart_rate import HeartRate, HeartRateData
+from ModelDefinition import SimpleNN  # Ensure to import your model class
 
-
+# Initialize Flask app and Bootstrap
 app = Flask(__name__)
 Bootstrap(app)
-csv_file = "music_characteristics.csv"
-# this is the csv file that will have the getcharacteristics
 
+# Global variables and constants
+script_dir = os.path.dirname(os.path.abspath(__file__))
+csv_file = "music_characteristics.csv"
+TIMEOUT = 60
+XX = 20
+HR = 0  # Global variable to store heart rate
+
+# Define the model
+input_size = 9
+hidden_size = 10
+output_size = 1
+learning_rate = 0.01
+dropout_prob = 0.1
+
+model_path = os.path.join(script_dir, "HBModel.pth")
+
+# Instantiate the model
+model = SimpleNN(input_size, hidden_size, output_size, dropout_prob)
+model.load_state_dict(torch.load(model_path))
+model.eval()
+
+# Route for the main page
 @app.route("/myapp")
 def index():
     music_files = get_music_files()
     return render_template('index.html', music_files=music_files)
 
+# Function to get music files
 def get_music_files():
-    rootpath = "/Users/jeremyjohn/Desktop/music" #where you put your file path for the song folder you want to use
+    rootpath = "/Users/jeremyjohn/Desktop/music"
     pattern = "*.mp3"
     files = glob.glob(os.path.join(rootpath, pattern))
     return [{'file_name': os.path.basename(file), 'file_path': file} for file in files]
 
+# Function that load audio segment
 def load_audio_segment(audioInfo, samplingRate, startTime, durationTime):
     return audioInfo[int(startTime * samplingRate):int((startTime + durationTime) * samplingRate)]
 
+# Function that compute tempo
 def compute_tempo(audioInfo, samplingRate):
     try:
         tempo, _ = librosa.beat.beat_track(y=audioInfo, sr=samplingRate)
@@ -40,6 +65,7 @@ def compute_tempo(audioInfo, samplingRate):
         print(f"Error computing tempo: {e}")
         return None
 
+# Function that compute average pitch
 def compute_average_pitch(audioInfo, samplingRate):
     try:
         pitches, _ = librosa.piptrack(y=audioInfo, sr=samplingRate)
@@ -48,6 +74,7 @@ def compute_average_pitch(audioInfo, samplingRate):
         print(f"Error computing pitch: {e}")
         return None
 
+# Function to gives the  characteristics of a music file
 def get_characteristics(filepath):
     try:
         y, samplingRate = librosa.load(filepath)
@@ -74,6 +101,7 @@ def get_characteristics(filepath):
         print(f"Error processing file {filepath}: {e}")
         return [None] * 7
 
+# Function to generate CSV with audio characteristics
 def generate_csv(directory_path, csv_file):
     pattern = "*.mp3"
     audio_files = glob.glob(os.path.join(directory_path, pattern))
@@ -88,12 +116,103 @@ def generate_csv(directory_path, csv_file):
             writer.writerow([os.path.basename(audio_file)] + characteristics)
             print(f"Characteristics for {audio_file}: {characteristics}")
 
+# Function to get resting heart rate
+def get_resting_HR():
+    start_time = time.time()
+    heart_rates = []
+
+    while time.time() - start_time < TIMEOUT:
+        current_rate = get_current_heart_rate()
+        heart_rates.append(current_rate)
+
+        if len(heart_rates) > 1 and (max(heart_rates) - min(heart_rates)) <= 3:
+            return int(np.mean(heart_rates))
+
+        time.sleep(XX)
+
+    return -1
+
+# Function to get current heart rate
+def get_current_heart_rate(device_id=0):
+    global HR
+    while True:
+        try:
+            node = Node()
+            node.set_network_key(0x00, ANTPLUS_NETWORK_KEY)
+            device = HeartRate(node, device_id=device_id)
+            break
+        except Exception as e:
+            print("Failed to initialize node, trying again!")
+
+    def on_found():
+        print(f"Device {device} found and receiving")
+
+    def on_device_data(page: int, page_name: str, data):
+        global HR
+        if isinstance(data, HeartRateData):
+            print(data.heart_rate)
+            HR = data.heart_rate
+
+    device.on_found = on_found
+    device.on_device_data = on_device_data
+
+    try:
+        print(f"Starting {device}, press Ctrl-C to finish")
+        node.start()
+    except Exception as e:
+        print("Closing ANT+ device...")
+    finally:
+        device.close_channel()
+        node.stop()
+    return HR
+
+# Function to select music based from heart rate
+def selectMusic(targetHR, heartRate, restingHR, csvLocation):
+    goalHRChange = targetHR - heartRate
+    with open(csvLocation, 'r') as file:
+        reader = csv.reader(file)
+        rows = list(reader)
+    del rows[0]
+    
+    currentSong = [rows[0][0], 0]
+    for musicRow in rows:
+        tempList = [float(musicRow[i]) for i in range(1, len(musicRow))]
+        tempList.append(heartRate)
+        tempList.append(restingHR)
+        tempTensor = torch.tensor(tempList, dtype=torch.float32)
+        tempprediction = model(tempTensor).tolist()[0]
+        tempprediction = [musicRow[0], tempprediction]
+        if goalHRChange > 0:
+            if tempprediction[1] > currentSong[1]:
+                currentSong = tempprediction.copy()
+        else:
+            if tempprediction[1] < currentSong[1]:
+                currentSong = tempprediction.copy()
+    return currentSong[0]
+
+# Route that starts heart rate monitoring
+@app.route("/start_hr_monitor", methods=["GET"])
+def start_hr_monitor():
+    hr = get_resting_HR()
+    return jsonify({"hr": hr})
+
+# Route to get music recommendations based on heart rate
+@app.route("/get_music", methods=["POST"])
+def get_music():
+    data = request.json
+    targetHR = data['targetHR']
+    heartRate = data['heartRate']
+    restingHR = data['restingHR']
+    csvLocation = os.path.join(script_dir, "music_characteristics.csv")
+    selected_music = selectMusic(targetHR, heartRate, restingHR, csvLocation)
+    return jsonify({'selected_music': selected_music})
 
 if __name__ == "__main__":
-    directory_path = "/Users/jeremyjohn/Desktop/music"  # the directory path that has  audio files
+    directory_path = "/Users/jeremyjohn/Desktop/music"
     
-    # Generate CSV file with audio get_characteristics
+    # Generate CSV file with audio characteristics
     generate_csv(directory_path, csv_file)
+
     def open_browser():
         webbrowser.open_new("http://127.0.0.1:5000/myapp")
     
@@ -102,22 +221,11 @@ if __name__ == "__main__":
     
     # Run the Flask application
     app.run(port=5000, debug=True)
-    
-    #app.run(port=5000)
-    #app.run(port=5000, debug=True)
-    
-    # this opens the browser after starting the Flask app
-   # def open_browser():
-        #webbrowser.open_new("http://127.0.0.1:5000/myapp")
-        #the flask app is running on http://127.0.0.1.5000
-    
-    # Starts a timer to get the browser open
-    #Timer(1, open_browser).start()
-    
-    # Run the Flask application
-    #app.run(port=5000, debug=True)
-    #flask_thread = Thread(target=run_flask_app)
-    #flask_thread.start()
+
+
+
+
+
         
     
 
