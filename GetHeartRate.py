435db22354714046bada4361ab2c51374a73ad48
Shah23200 <160190546+Shah23200@users.noreply.github.com>
2024-07-01 15:54:47 -0400

    Class changes

--- GetHeartRate.py
+++ GetHeartRate.py
@@ -1,6 +1,7 @@
 import threading
 import time
-from heartRateFeed import start_monitor, heart_rate_data
+import numpy as np
+from HeartRateFeed import start_monitor, heart_rate_data
 
 class HeartRateReader:
     def __init__(self):
@@ -22,7 +23,26 @@ class HeartRateReader:
             time.sleep(1)
 
     def get_heart_rate(self):
-        return self.current_heart_rate
+        return self.read_heart_rate()
+    def get_heart_rate_int(self):
+        return self.read_heart_rate()[0]
+    def get_resting_heart_rate(self):
+                
+        TIMEOUT = 60
+        start_time = time.time()
+  
+        resting_heart_rate = None  
+     
+        while(True):
+            heart_rates = self.read_heart_rate()
+            if len(heart_rates) > 1 and (max(heart_rates) - min(heart_rates)) <= 3:
+                resting_heart_rate = int(np.mean(heart_rates))  
+                return resting_heart_rate
+                
+            if (time.time() - start_time) >= TIMEOUT:
+                resting_heart_rate = -1  
+                return resting_heart_rate
+
 
 if __name__ == "__main__":
     reader = HeartRateReader()
