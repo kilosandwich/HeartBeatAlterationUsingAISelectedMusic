43f293123e917596cd398196cc8ff6df9bf52167
Phillip Wandyez <wandyez@gmail.com>
2024-07-02 21:39:38 -0400

    Fixed GenerateCSV
    Fixed issue where GenerateCSV would have an error when the CSV did not exist or was blank using a try except loop.

--- GenerateCSV.py
+++ GenerateCSV.py
@@ -4,16 +4,21 @@ import glob
 from GetCharacteristics import get_characteristics
 
 def generate_csv(directory_path, csv_file):
+    print("Music directory is: ", directory_path, "the CSV file to save to is: ", csv_file)
     pattern = "*.mp3"
     audio_files = glob.glob(os.path.join(directory_path, pattern))
     processed_files = set()
     if os.path.exists(csv_file):
         with open(csv_file, mode='r') as file:
             reader = csv.reader(file)
-            next(reader)  
-            for row in reader:
-                processed_files.add(row[0])
-        print("These are the files we found in: ", directory_path, " that we tried to write to ", csv_file)
+            #if the file is not blank, then see what files already exist. 
+            try:
+                next(reader)  
+                for row in reader:
+                    processed_files.add(row[0])
+            except Exception as e:
+                print("CSV is blank! No files to skip!")
+        print("These are the files ALREADY found in: ", csv_file, " that we tried to write to ", csv_file)
         print(processed_files)
         print("=================================================")
 
@@ -28,3 +33,6 @@ def generate_csv(directory_path, csv_file):
             characteristics = get_characteristics(audio_file)
             rowToWrite = [musicFilePath] + characteristics
             writer.writerow(rowToWrite)
+            
+if __name__ == "__main__":
+    generate_csv("music", "music_characteristics.csv" )
