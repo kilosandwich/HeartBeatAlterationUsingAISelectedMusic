8acdbef629db1b487d0732999459fca8f9ee9296
Phillip Wandyez <wandyez@gmail.com>
2024-07-02 21:26:06 -0400

    Add files via upload
    Added code commenting to get characteristics, these comments will help determine errors in generateCSV

--- GetCharacteristics.py
+++ GetCharacteristics.py
@@ -5,20 +5,24 @@ def load_audio_segment(audioInfo, samplingRate, startTime, durationTime):
     return audioInfo[int(startTime * samplingRate):int((startTime + durationTime) * samplingRate)]
 
 def compute_tempo(audioInfo, samplingRate):
-    return librosa.beat.beat_track(y=audioInfo, sr=samplingRate)[0][0]
+    return librosa.beat.beat_track(y=audioInfo, sr=samplingRate)[0]
 
 def compute_average_pitch(audioInfo, samplingRate):
     pitches, _ = librosa.piptrack(y=audioInfo, sr=samplingRate)
     return np.mean(pitches[pitches > 0])
 
 def get_characteristics(filepath):
+    print("I am attempting to analyze: ", filepath)
     y, samplingRate = librosa.load(filepath)
+    print("Getting length")
     songLength = librosa.get_duration(y=y, sr=samplingRate)
+    print("Getting tempo")
     avgTempo = compute_tempo(y, samplingRate)
     first_30s_audio = load_audio_segment(y, samplingRate, 0, 30)
     last_30s_audio = load_audio_segment(y, samplingRate, max(0, songLength - 30), 30)
     tempo_first_30 = compute_tempo(first_30s_audio, samplingRate)
     tempo_last_30 = compute_tempo(last_30s_audio, samplingRate)
+    print("Getting pitch")
     avgPitch = compute_average_pitch(y, samplingRate)
     pitch_first_30 = compute_average_pitch(first_30s_audio, samplingRate)
     pitch_last_30 = compute_average_pitch(last_30s_audio, samplingRate)
@@ -45,6 +49,7 @@ def get_characteristics(filepath):
 
 #TEST IT
 if __name__ == "__main__":
+    print("I have started up!")
     filepath = "Nixon.mp3"
     characteristics = get_characteristics(filepath)
     print(characteristics)
