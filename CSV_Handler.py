5765e96ed31d642ba918b534fdb046a25b185720
Shah23200 <160190546+Shah23200@users.noreply.github.com>
2024-07-22 14:23:06 -0400

    Add files via upload

--- CSV_Handler.py
+++ CSV_Handler.py
@@ -11,47 +11,62 @@ program runs faster.
 """
 import threading
 from GenerateCSV import generate_csv
+
 class CSV_Handler():
-    def __init__(self, music_dir: str, CSV_dir: str,):
-        #On creation of this class, with the music folder and CSV directory as an input,
-        #run generate CSV in a separate thread. This will mean that whichever program
-        #uses this class will have the ability to continue operating without pause once 
-        #the thread is initialized
+    def __init__(self, music_dir: str, CSV_dir: str):
+        # On creation of this class, with the music folder and CSV directory as an input,
+        # run generate CSV in a separate thread. This will mean that whichever program
+        # uses this class will have the ability to continue operating without pause once 
+        # the thread is initialized
         
-        #keep in mind that running this COULD mean that there are race conditions 
-        #if the writing is not finished in time, this should not be an issue since
-        #generate CSV does not consider files that it has already consider,
-        #and that new files when added via the front end runs generate CSV every time.
+        # keep in mind that running this COULD mean that there are race conditions 
+        # if the writing is not finished in time, this should not be an issue since
+        # generate CSV does not consider files that it has already consider,
+        # and that new files when added via the front end runs generate CSV every time.
         
-        #save the initialization inputs for later
+        # save the initialization inputs for later
         self.music_dir = music_dir
         self.CSV_dir = CSV_dir
+        self.is_running = False  # Add a flag to check if the thread is already running
+        self.lock = threading.Lock()  # Add a lock for thread safety
         
-        #start the thread that runs generate_csv
+        # start the thread that runs generate_csv
+        print("CSV_Handler instantiated")
         self.createThread()
+
     def createThread(self):
-        #This creates a thread for the specified music directory and csv directory
-        print("You have added a new song! Generating new CSV")
-        print("CSV handler thread created, running generate_csv with arguments: ", self.music_dir, " ", self.CSV_dir)
-        self.thread = threading.Thread(target=generate_csv, args=(self.music_dir, self.CSV_dir))
-        self.thread.daemon = True #shut down the thread once it is done to conserve resources
-        self.thread.start()
-    def songAdded(self):
-        #theoretically, if this function is called while createThread is running, this will cause an error, therefore
-        #the best solution is to FORCE the program to wait while songs are added.
-        #this will be slower, but it will prevent the player from doing anything else that could cause a race condition.
+        with self.lock:
+            if self.is_running:
+                print("CSV generation is already running. Skipping new thread creation.")
+                return
+            self.is_running = True
+            print("You have added a new song! Generating new CSV")
+            print("CSV handler thread created, running generate_csv with arguments: ", self.music_dir, " ", self.CSV_dir)
+            self.thread = threading.Thread(target=self.run_generate_csv)
+            self.thread.daemon = True  # shut down the thread once it is done to conserve resources
+            self.thread.start()
+
+    def run_generate_csv(self):
         generate_csv(self.music_dir, self.CSV_dir)
-    
-        
-#local debugging feature
+        with self.lock:
+            self.is_running = False  # Resets the flag when it is done -_-
+
+    def songAdded(self):
+        with self.lock:
+            if self.is_running:
+                print("CSV generation is already running, we are waiting for it to finish")
+                self.thread.join()
+            generate_csv(self.music_dir, self.CSV_dir)
+
+# Local debugging feature
 if __name__ == "__main__":
     import os
-    #find the directory this file is currently in
+    # Find the directory this file is currently in
     script_dir = os.path.dirname(os.path.abspath(__file__))
     music_dir = "music"
     csv_dir = "music_characteristics.csv"
 
-    #get the absolute directory for the music folder one sublevel down
+    # Get the absolute directory for the music folder one sublevel down
     music_dir = os.path.join(script_dir, music_dir)
     csv_dir = os.path.join(script_dir, csv_dir)
-    Data = CSV_Handler(music_dir,csv_dir)
+    Data = CSV_Handler(music_dir, csv_dir)
--- GenerateCSV.py
+++ GenerateCSV.py
@@ -2,49 +2,54 @@ import os
 import csv
 import glob
 from GetCharacteristics import get_characteristics
-#Tell the user this function only accepts strings
-def generate_csv(directory_path: str, csv_file: str):
-    print("Music directory is: ", directory_path, "the CSV file to save to is: ", csv_file)
-    pattern = "*.mp3"
-    audio_files = glob.glob(os.path.join(directory_path, pattern))
-    processed_files = set()
-    if os.path.exists(csv_file):
-        with open(csv_file, mode='r') as file:
-            reader = csv.reader(file)
-            #if the file is not blank, then see what files already exist. 
-            try:
-                next(reader)  
-                for row in reader:
-                    print(row[0])
-                    processed_files.add(row[0])
-            except Exception as e:
-                print("CSV is blank! No files to skip!")
-        print("These are the files ALREADY found in: ", directory_path, " that we tried to write to ", csv_file)
-        print(processed_files)
-        print("=================================================")
+from threading import Lock
 
-    with open(csv_file, mode='a', newline='') as file:
-        writer = csv.writer(file)
-        if not processed_files:
-            writer.writerow(["File", "Song Length", "Average Tempo", "Tempo First 30s", "Tempo Last 30s", "Average Pitch", "Pitch First 30s", "Pitch Last 30s"])
-        for audio_file in audio_files:
-            musicFilePath = os.path.basename(audio_file)
-            if musicFilePath in processed_files:
-                continue
-            characteristics = get_characteristics(audio_file)
-            rowToWrite = [musicFilePath] + characteristics
-            writer.writerow(rowToWrite)
-            
+csv_lock = Lock()  # Creates a lock for the CSV which is used later
+
+# Tell the user this function only accepts strings
+def generate_csv(directory_path: str, csv_file: str):
+    with csv_lock:
+        print("Music directory is: ", directory_path, "the CSV file to save to is: ", csv_file)
+        pattern = "*.mp3"
+        audio_files = glob.glob(os.path.join(directory_path, pattern))
+        processed_files = set()
+        if os.path.exists(csv_file):
+            with open(csv_file, mode='r') as file:
+                reader = csv.reader(file)
+                # If the file is not blank, then see what files already exist. 
+                try:
+                    next(reader)
+                    for row in reader:
+                        print(row[0])
+                        processed_files.add(row[0])
+                except Exception as e:
+                    print("CSV is blank! No files to skip!")
+            print("These are the files ALREADY found in: ", directory_path, " that we tried to write to ", csv_file)
+            print(processed_files)
+            print("=================================================")
+
+        with open(csv_file, mode='a', newline='') as file:
+            writer = csv.writer(file)
+            if not processed_files:
+                writer.writerow(["File", "Song Length", "Average Tempo", "Tempo First 30s", "Tempo Last 30s", "Average Pitch", "Pitch First 30s", "Pitch Last 30s"])
+            for audio_file in audio_files:
+                musicFilePath = os.path.basename(audio_file)
+                if musicFilePath in processed_files:
+                    continue
+                characteristics = get_characteristics(audio_file)
+                rowToWrite = [musicFilePath] + characteristics
+                writer.writerow(rowToWrite)
+
 if __name__ == "__main__":
     import os
-    #debugging program to ONLY run when this function is the only one running
+    # Debugging program to ONLY run when this function is the only one running
 
-    #find the directory this file is currently in
+    # Find the directory this file is currently in
     script_dir = os.path.dirname(os.path.abspath(__file__))
     music_dir = "music"
     csv_dir = "music_characteristics.csv"
 
-    #get the absolute directory for the music folder one sublevel down
+    # Get the absolute directory for the music folder one sublevel down
     music_dir = os.path.join(script_dir, music_dir)
     csv_dir = os.path.join(script_dir, csv_dir)
-    generate_csv(music_dir, csv_dir )
+    generate_csv(music_dir, csv_dir)
